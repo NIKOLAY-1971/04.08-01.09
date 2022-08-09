04539a0a674f8c8e466fd3915ced53ffe60849b2
Nikolay <rnm71@yandex.ru>
2022-08-09 15:57:54 +0400

    4 option

--- task9/Program.cs
+++ task9/Program.cs
@@ -7,12 +7,24 @@
 
 int number = new Random().Next(10, 100);
 // Console.WriteLine($"Случайное число из диапазона 10-99 {number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 // if (firstDigit == secondDigit) Console.WriteLine("цифры равны");
 // else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая часть числа {number} -> {firstDigit}");
 // else Console.WriteLine($"Наибольшая часть числа {number} -> {secondDigit}");
 
-string result = firstDigit == secondDigit ? "цифры равны" 
-: firstDigit > secondDigit ? firstDigit.ToString() : secondDigit.ToString();
+// string result = firstDigit == secondDigit ? "цифры равны"
+// : firstDigit > secondDigit ? firstDigit.ToString() : secondDigit.ToString();
+// Console.WriteLine($"Наибольшая часть числа {number} -> {result}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit; 
+    return result; //return firstDigit > secondDigit ? firstDigit : secondDigit; (без предыдущей строки)
+}
+int result = MaxDigit(number);
+int result2 = MaxDigit(56); //проверка
 Console.WriteLine($"Наибольшая часть числа {number} -> {result}");
+Console.WriteLine($"Наибольшая часть числа {56} -> {result2}"); //проверка
