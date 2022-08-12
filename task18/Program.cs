42074dc1eb1197d63b09c6c624d925def08293ad
Nikolay <rnm71@yandex.ru>
2022-08-12 22:14:10 +0400

    added a task18

---
+++ task18/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти
+
+Console.Write("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string CoordinateRangee(int quar)
+{
+    if (quar == 1) return "X>0, Y>0";
+    if (quar == 2) return "X<0, Y>0";
+    if (quar == 3) return "X<0, Y<0";
+    if (quar == 4) return "X>0, Y<0";
+    return "Введено неверное число (ввести от 1 до 4)";
+}
+string result = CoordinateRangee(quarter);
+Console.WriteLine(result);
