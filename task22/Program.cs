351700923dc91b10d3a2ba6e9472d3aad6ce2913
Nikolay <rnm71@yandex.ru>
2022-08-12 23:30:55 +0400

    added a task22

---
+++ task22/Program.cs
@@ -0,0 +1,14 @@
+﻿// 22. Напишите программу, которая принимает на вход число (N) и
+// выдает таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите положительное число N");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1) Console.WriteLine("Введено не корректное число");
+else
+{
+    for (int i = 1; i <= n; i++)
+    {
+        int SquareNumber = i * i;
+        Console.WriteLine($"{i}  {SquareNumber}");
+    }
+}
