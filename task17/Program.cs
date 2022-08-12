8a191954253867a6d748f34678c09862481266b5
Nikolay <rnm71@yandex.ru>
2022-08-12 20:30:54 +0400

    added a task17 with the method

---
+++ task17/Program.cs
@@ -0,0 +1,25 @@
+﻿// 17. Показать в какой четверти находится точка по введенным координатам
+
+
+Console.WriteLine("Введите координаты точки:");
+Console.WriteLine("X:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+// if (x > 0 && y > 0) Console.WriteLine("Первая четверть");
+// else if (x < 0 && y > 0) Console.WriteLine("Вторая четверть");
+// else if (x < 0 && y < 0) Console.WriteLine("Третья четверть");
+// else if (x > 0 && y < 0) Console.WriteLine("Четвертая четверть");
+// else Console.WriteLine("Введены не корректные координаты");
+
+string GetQuater(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return "Первая четверть";
+    if (xc < 0 && yc > 0) return "Вторая четверть";
+    if (xc < 0 && yc < 0) return "Третья четверть";
+    if (xc > 0 && yc < 0) return "Четвертая четверть";
+    return "Введены не корректные координаты";
+}
+string result = GetQuater(x, y);
+Console.WriteLine(result);
