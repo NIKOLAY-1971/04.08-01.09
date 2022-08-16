2d21a89903b25ed12d20f9c66ad4ef80930da9c7
Nikolay <rnm71@yandex.ru>
2022-08-16 20:16:57 +0400

    added a new task30

--- task30/Program.cs
+++ task30/Program.cs
@@ -2,8 +2,9 @@
 // заполненный нулями и еденицами в случайном порядке
 
 int[] array = new int[8];
+Random rnd = new Random();
 for (int i = 0; i < 7; i++)
 {
-    array[i] = new Random().Next(0,2);
+    array[i] = rnd.Next(0,2);
     Console.Write(array[i]+"   ");
 }
