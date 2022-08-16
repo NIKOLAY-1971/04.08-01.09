1c20dfa84569aa2bed465508428536e1dc9463af
Nikolay <rnm71@yandex.ru>
2022-08-16 18:10:12 +0400

    added a task26

---
+++ task26/Program.cs
@@ -0,0 +1,13 @@
+﻿// 26. Напищите программу, которая принимает на вход число и
+// выдает количество цифр в числе
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int n = num;
+int NumberCharacters = 0;
+while (n > 0)
+{
+    n = n / 10;
+    NumberCharacters++;
+}
+ Console.WriteLine($"Количество цифр в числе {num} равно {NumberCharacters}");
