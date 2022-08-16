85a984ad948a873d1549493ba29d5ab9b3005b9e
Nikolay <rnm71@yandex.ru>
2022-08-16 18:28:38 +0400

    added a task28

---
+++ task28/Program.cs
@@ -0,0 +1,11 @@
+﻿// 28. Напишите программу, которая прнимает на вход число N и
+// выдает произведение чисел от 1 до N
+
+Console.WriteLine("Введите целое положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int composition = 1;
+for (int i = 1; i <= n; i++)
+{
+    composition = composition * i;
+}
+Console.WriteLine($"Произведение чисел от 1 до {n} равно {composition}");
