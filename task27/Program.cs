0c350cc004d7692b97c482a94a74d33c612382cf
Nikolay <rnm71@yandex.ru>
2022-08-16 21:14:01 +0400

    added a task27

---
+++ task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите натуральное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int n = num;
+int count = 0;
+int summa = 0;
+while (n > 0)
+{
+    summa = summa + n % 10;
+    n = n / 10;
+    count++;
+}
+Console.WriteLine($"Сумма цифр числа {num} равна {summa}");
