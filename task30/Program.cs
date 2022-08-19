2de51c206014efad0b3b101430ba4967c2690e33
Nikolay <rnm71@yandex.ru>
2022-08-19 14:15:26 +0400

    added a task30

--- task30/Program.cs
+++ task30/Program.cs
@@ -3,7 +3,7 @@
 
 int[] array = new int[8];
 Random rnd = new Random();
-for (int i = 0; i < 7; i++)
+for (int i = 0; i <= 7; i++)
 {
     array[i] = rnd.Next(0,2);
     Console.Write(array[i]+"   ");
