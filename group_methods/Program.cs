2c0f69387912020c1f3f5ea4da45b42455792003
Nikolay <rnm71@yandex.ru>
2022-08-19 18:44:44 +0400

    Method2

--- group_methods/Program.cs
+++ group_methods/Program.cs
@@ -4,4 +4,25 @@ void Method1()
 {
     Console.WriteLine("Автор...");
 }
-Method1();
+// Method1();
+
+
+
+// 2 Метод: принимают, но ничего не возвращают
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+
+void Method21(string msg, int count) // явное указание аргумента - поименнованное
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i ++;
+    }
+}
+Method21("Текст", 4); // 4 раза выводит слово Текст
+Method21(count: 4, msg: "Новый текст"); // 4 раза выводит слово Новый текст
