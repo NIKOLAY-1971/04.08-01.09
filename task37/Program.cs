06d9db0ecfb34103dd1e12dfb46261bbb6c9290d
Nikolay <rnm71@yandex.ru>
2022-08-21 20:18:14 +0400

    task37

---
+++ task37/Program.cs
@@ -0,0 +1,47 @@
+﻿// 37. Найдите произведение пар чисел в одномерном массиве. 
+// Парой считается первый и последний элемент, второй и предпоследний и т.д.
+// Результат запишите в новом массиве.
+
+int[] CreateArrayRndInt(int size, int min, int max) // Метод создания массива
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array) // Метод вывода массива
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите количество элементов одномерного массива");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArrayRndInt(6, 1, 10);
+PrintArray(arr);
+
+
+int ProdPairsNumbers(int[] arr) //Произведение крайних пар чисел
+{
+    int length1 = 0;
+    if (array.Length % 2 == 0) length1 = array.Length / 2;
+    else length1 = array.Length / 2 + 1;
+    int[] array1 = new int[length1];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        array1[i] = array[i] * array[array.Length - i - 1];
+        if (i + 1 > length1) array1[i + 1] = array[array.Length / 2];
+    }
+    return array1;
+}
+int[] arr1 = ProdPairsNumbers(array);
+PrintArray(arr1);
