c4dee4e5773853d76f2bb7c14a398353d75627ca
Nikolay <rnm71@yandex.ru>
2022-08-24 14:14:07 +0400

    added a task42

---
+++ task42/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+
+Console.WriteLine("Введите число:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int res = 0;
+int mult = 1;
+while (x > 0)
+{
+    res += x % 2 * mult;
+    x = x / 2;
+    mult *=10;
+}
+Console.WriteLine(res);
