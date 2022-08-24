673956128fe6b86f6c546b2efb85b90dd7e8d900
Nikolay <rnm71@yandex.ru>
2022-08-24 21:30:07 +0400

    added a task41

---
+++ task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int SumNumGreaterZero(int numbers)
+{
+    int summa = 0;
+    int num = 0;
+    for (int i = 0; i < numbers; i++)
+    {
+        Console.Write($"Введите {i + 1} проверяемое число: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num > 0) summa++;
+    }
+    return summa;
+}
+Console.WriteLine("Введите необходимое количество для проверки чисел:");
+int m = Convert.ToInt32(Console.ReadLine());
+int result = SumNumGreaterZero(m);
+Console.WriteLine($"Количество чисел больше нуля = {result}");
