73b503ee45f773ff7ca9342d48868b4cd39e6848
Nikolay <rnm71@yandex.ru>
2022-08-24 18:07:56 +0400

    added a task42

--- task42/Program.cs
+++ task42/Program.cs
@@ -2,15 +2,25 @@
 // 45 -> 101101
 // 3 -> 11
 
-Console.WriteLine("Введите число:");
-int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число:");
+// int x = Convert.ToInt32(Console.ReadLine());
 
-int res = 0;
-int mult = 1;
-while (x > 0)
+// int res = 0;
+// int mult = 1;
+// while (x > 0)
+// {
+//     res += x % 2 * mult;
+//     x = x / 2;
+//     mult *=10;
+// }
+// Console.WriteLine(res);
+
+Console.WriteLine("Введите число:"); // Рекурсия
+int x = Convert.ToInt32(Console.ReadLine());
+void DecToBin(int num)
 {
-    res += x % 2 * mult;
-    x = x / 2;
-    mult *=10;
+    if(num == 0) return;
+    DecToBin(num / 2);
+    Console.Write(num % 2); 
 }
-Console.WriteLine(res);
+DecToBin(x);
