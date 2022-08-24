4ca0c6db3e63381d89deda780b2990b605f75c5d
Nikolay <rnm71@yandex.ru>
2022-08-25 01:23:26 +0400

    added a task43

--- task43/Program.cs
+++ task43/Program.cs
@@ -2,7 +2,7 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-double[] T(double a1, double c1, double a2, double c2)
+double[] IntersectionPoint(double a1, double c1, double a2, double c2)
 {
     double x = Math.Round((c1 - c2) / (a2 - a1), 1);
     double y = a1 * x + c1;
@@ -18,5 +18,5 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите свободный коэффициент b2 второй прямой");
 double b2 = Convert.ToDouble(Console.ReadLine());
 
-double[] result = T(k1, b1, k2, b2);
+double[] result = IntersectionPoint(k1, b1, k2, b2);
 Console.WriteLine($"Точка пересечения двух прямых: ({result[0]}; {result[1]})");
