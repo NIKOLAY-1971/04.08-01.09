e1dc4feee842cb34bafade683f7e5ff265661a5e
Nikolay <rnm71@yandex.ru>
2022-08-26 17:06:30 +0400

    added a task49

---
+++ task49/Program.cs
@@ -0,0 +1,51 @@
+﻿// 49. Задайте двумерный массив. Найдите элементы у которых оба индекса четные, 
+// и заменити эти элементы на их квадраты.
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max) // Метод создания массива
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix) // Метод вывода массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, "); // 3 - размер вывода числа
+            // else Console.Write($"{matrix[i, j],3}");
+            Console.Write(j < matrix.GetLength(1) - 1
+            ? $"{matrix[i, j],3}, " : $"{matrix[i, j],3}");
+
+        }
+        Console.WriteLine("]");
+    }
+    Console.WriteLine("");
+}
+int[,] ReplacingEvenElements(int[,] matrix) // Замена четных элементов массива квадратами элементов
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+    return matrix;
+}
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+int[,] array12D = ReplacingEvenElements(array2D);
+PrintMatrix(array12D);
