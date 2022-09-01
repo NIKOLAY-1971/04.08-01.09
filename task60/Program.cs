a17d83927233d9436b9fbc6be687856b72e62a49
Nikolay <rnm71@yandex.ru>
2022-09-01 15:33:00 +0300

    added a task60

---
+++ task60/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] CreateMatrixRndInt(int row, int col, int dep, int min, int max) // Метод создания массива
+{
+    int[,,] matrix = new int[row, col, dep];
+    Random rnd = new Random();
+    int[] a = new int[matrix.Length];
+    a[0] = rnd.Next(min, max);
+    int k = 0;
+    for (int i = 1; i < a.Length; i++) // Проверка совпадения числа, получение неповторяющихся чисел
+    {
+    link:
+        a[i] = rnd.Next(min, max);
+
+        for (int t = 0; t < i; t++)
+        {
+            if (a[t] == a[i]) goto link;
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int d = 0; d < matrix.GetLength(2); d++)
+            {
+                matrix[i, j, d] = a[k];
+                k++;
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix) // Метод вывода массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int d = 0; d < matrix.GetLength(2); d++)
+            {
+                Console.WriteLine($"{matrix[i, j, d]},({i},{j},{d})");
+            }
+        }
+    }
+    Console.WriteLine("");
+}
+
+int[,,] array3D = CreateMatrixRndInt(2, 2, 2, 10, 100);
+PrintMatrix(array3D);
---
+++ temp/Program.cs
@@ -0,0 +1,69 @@
+﻿// 
+
+// int[] a =  {1,6,6,4,5};
+// for (int i = 0; i < a.Length; i++)
+// {
+//     Console.WriteLine(a[i]);
+// }
+// int imax = 0;
+// for (int k = 0; k < a.Length; k++)
+// {
+//   int max = a[k];
+
+// for (int i = k; i < a.Length; i++)
+// {
+//     if (max<=a[i])
+//     {
+//         max = a[i];
+//         imax = i;
+//     }
+// }
+// max = a[k];
+// a[k] = a[imax];
+// a[imax] = max;
+
+
+// for (int i = 0; i < a.Length; i++)
+// {
+//     Console.Write(a[i]);
+// }
+// Console.WriteLine("");
+// }
+
+// Random rnd = new Random();
+// int[] a = new int[8];
+// for (int i = 0; i < 8; i++)
+// {
+//     a[i] = rnd.Next(1, 9 + 1);
+//     Console.WriteLine(a[i]);
+// }
+
+// for (int i = 0; i < 8; i++)
+// {
+//     Console.WriteLine(a[i]);
+// }
+
+Random rnd = new Random();
+int[] a = new int[10];
+a[0] = rnd.Next(10, 100);
+
+for (int i = 1; i < 10; i++)
+{
+    a[i] = rnd.Next(10, 100);
+link:
+int k = 0;
+    for (int j = 0; j < i; j++)
+    {
+        if (a[j] == a[i]) 
+        {
+            a[j] = rnd.Next(10, 100);
+        k=1;
+        }
+        else k = 0;
+    }
+    if (k==1) goto link;
+    
+
+
+    Console.WriteLine(a[i]);
+}
