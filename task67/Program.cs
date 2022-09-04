8c2955a3fd9fc0f3738863b491a71c83b8621022
Nikolay <rnm71@yandex.ru>
2022-09-04 19:12:24 +0300

    added a task67

---
+++ task67/Program.cs
@@ -0,0 +1,14 @@
+﻿// 67. Напишите программу, которая на вход 
+// принимает число и возвращает сумму его цифр.
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigital(int num)
+{
+    int sum = num % 10;
+    if (num == 0) return sum;
+    return sum += SumDigital(num / 10);
+}
+int result = SumDigital(number);
+Console.WriteLine(result);
