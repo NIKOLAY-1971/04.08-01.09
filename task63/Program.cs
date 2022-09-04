c9f96858a12e521233c887f9767f5cd889a3dffa
Nikolay <rnm71@yandex.ru>
2022-09-04 17:28:24 +0300

    added a task63

---
+++ task63/Program.cs
@@ -0,0 +1,13 @@
+﻿// 63. Задайте значение N. Напишите программу, 
+// кторая выведет все натуральные числа от 1 до N.
+
+System.Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumber(int num)
+{
+if (num == 0) return;
+NaturalNumber(num - 1);
+Console.Write($"{num} ");
+}
+NaturalNumber(number);
