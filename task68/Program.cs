5e4d3da8a6a7a2dd36849eff00b8baed725b6a6b
Nikolay <rnm71@yandex.ru>
2022-09-05 21:44:21 +0300

    added a task68

--- task68/Program.cs
+++ task68/Program.cs
@@ -2,7 +2,7 @@
 // Даны два неотрицательных числа m и n.
 // m = 3, n = 2 -> A(m,n) = 29
 
-int AckermanFunction(int ma, int na)
+int AckermanFunction(int ma, int na) // Метод вычисления функции Аккермана
 {
     if (ma == 0) return na + 1;
     if (ma > 0 && na == 0) return AckermanFunction(ma - 1, 1);
